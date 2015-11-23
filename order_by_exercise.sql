5fd7168b737a334149b19c3427d9d0db43048836
Sj <sjmrstevens@gmail.com>
2015-11-23 14:20:04 -0600

    updated order by exercise 1 to sort and find Khun Barnini

--- order_by_exercise.sql
+++ order_by_exercise.sql
@@ -26,7 +26,8 @@ AND last_name LIKE "%e";
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 SELECT *
 FROM employees
