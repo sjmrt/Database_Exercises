8c2e3e5466f741dc8c034fd289ad40dbecfce376
Sj <sjmrstevens@gmail.com>
2015-11-23 14:14:41 -0600

    updated order by exercise 1 to search for last names, then first names, ASC

--- order_by_exercise.sql
+++ order_by_exercise.sql
@@ -5,7 +5,7 @@ FROM employees
 WHERE first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya'
-ORDER BY first_name ASC, last_name ASC;
+ORDER BY last_name ASC, first_name ASC;
 
 SELECT *
 FROM employees
