01f9c38550fbba027d5273d844b2f624121cdb28
Sj <sjmrstevens@gmail.com>
2015-11-23 15:36:13 -0600

    added query to find unique last names with q in them

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -7,3 +7,9 @@ FROM employees
 WHERE last_name LIKE "E%"
 AND last_name LIKE "%e"
 GROUP BY first_name, last_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
